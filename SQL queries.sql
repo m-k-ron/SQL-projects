d9c2bcffccac3dc57f5d47e58da4f1a7dbeaf1c0
Hubert Krajewski <krajewskihubert14@gmail.com>
2025-03-01 22:33:44 +0100

    Update SQL queries.sql

--- SQL queries.sql
+++ SQL queries.sql
@@ -1,4 +1,6 @@
---# Queries in MySQL
+/** Queries in MySQL **/
+
+/* menu_items table */
 
 -- 1. View the menu_items table.
 SELECT * FROM menu_items;
@@ -33,6 +35,8 @@ SELECT category, AVG(price) AS average_price
 FROM menu_items
 GROUP BY category;
 
+/* order_details table */
+
 -- 1. View the order_details table.
 SELECT * FROM order_details;
 
@@ -40,7 +44,7 @@ SELECT * FROM order_details;
 SELECT  MIN(order_date), MAX(order_date)
 FROM order_details
 
--- 3. How many Irders were made within this date range?
+-- 3. How many orders were made within this date range?
 SELECT COUNT(DISTINCT order_id) 
 FROM order_details;
 
@@ -69,24 +73,46 @@ FROM order_details
 GROUP BY order_id) AS avg_items
 ;
 
+/* combining both tables */
+
 -- 1. Combine the menu_items and order_details tables into a single table.
+/* checking both tables */
 SELECT * FROM menu_items;
 SELECT * FROM order_details;
+
 SELECT * FROM order_details od
-FULL JOIN menu_items mi
+LEFT JOIN menu_items mi
 ON od.item_id = mi.menu_item_id;
 
 -- 2. What were the least and most ordered items? What categories were they in?
-SELECT * FROM order_details od
-FULL JOIN menu_items mi
+SELECT item_name, COUNT(order_details_id) AS num_purchases
+FROM order_details od
+LEFT JOIN menu_items mi
 ON od.item_id = mi.menu_item_id
-WHERE 
+GROUP BY item_name
+ORDER BY num_purchases;
 
--- 3. Ifat were the top 5 orders that spent the most money?
-
+-- 3.What were the top 5 orders that spent the most money?
+SELECT order_id, SUM(price) AS total_spend
+FROM order_details od
+LEFT JOIN menu_items mi
+ON od.item_id = mi.menu_item_id
+GROUP BY order_id
+ORDER BY total_spend DESC
+LIMIT 5;
 
 -- 4. View the details of the highest spend order. What insights can you gather from the results?
-
+SELECT category, COUNT(item_id) AS num_items
+FROM order_details od
+LEFT JOIN menu_items mi
+ON od.item_id = mi.menu_item_id
+WHERE order_id = '440'
+GROUP BY category;
 
 -- 5. View the details of the top 5 highest spend orders. What insights can you gather from the results?
-```
+SELECT order_id, category, COUNT(item_id) AS num_items
+FROM order_details od
+LEFT JOIN menu_items mi
+ON od.item_id = mi.menu_item_id
+WHERE order_id IN ('440', '2075', '1956', '330', '2675')
+GROUP BY order_id, category;
